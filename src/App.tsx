8b470aba5798d8092eea9635b09dfcfcf14f6392
Brian Lagranda <brianlagranda@gmail.com>
2024-09-05 15:02:33 -0300

    feat: Add footer and some hover effects for links

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,28 @@ import BlogCard from './components/BlogCard';
 function App() {
   return (
     <>
-      <h1 className='text-center text-3xl font-bold font-figtree mb-6 text-wrap w-64 mx-auto'>
-        Blog Preview Card Frontendmentor.io
-      </h1>
-      <BlogCard />
+      <header className='select-none'>
+        <h1 className='text-center text-3xl font-bold font-figtree mb-6 text-wrap w-64 mx-auto'>
+          Blog Preview Card{' '}
+          <a href='https://www.frontendmentor.io/' className='hover:text-white'>
+            Frontendmentor.io
+          </a>
+        </h1>
+      </header>
+      <main>
+        <BlogCard />
+      </main>
+      <footer>
+        <p className='text-center text-xl font-bold font-figtree mt-6'>
+          Coded by{' '}
+          <a
+            href='https://github.com/brianlagranda'
+            className='hover:text-white'
+          >
+            Brian Lagranda
+          </a>
+        </p>
+      </footer>
     </>
   );
 }
